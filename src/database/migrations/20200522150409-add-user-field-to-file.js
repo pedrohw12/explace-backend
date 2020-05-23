629c441860ecfe05572e63d11fd34ecbbf4538b8
Pedro Wanderley <peu-zico>
2020-05-23 13:30:48 -0300

    adding comments

--- src/database/migrations/20200522150409-add-user-field-to-file.js
+++ src/database/migrations/20200522150409-add-user-field-to-file.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn(
       'files', // qual tabela que eu quero adicionar uma coluna
-      'city_id', // qual o nome da coluna que vai ser adicionada
+      'city_id', // qual o nome da coluna que vai ser adicionada // Quero add a coluna city_id na tabela files
       {
         type: Sequelize.INTEGER, // integer porque vamos referenciar apenas o ID da imagem
         references: { model: 'cities', key: 'id' },  // a foreing key
@@ -23,12 +23,7 @@ module.exports = {
 // {
 //   type: Sequelize.INTEGER, // integer porque vamos referenciar apenas o ID da imagem
 
-//   references: { model: 'files', key: 'id' },  // a foreing key
+//   references: { model: 'cities', key: 'id' },  // a foreing key
 //   # model - qual o nome da tabela dentro de model 
-//   |_ por exemplo, eu quero que este campo referencie a tabela files
-//   # key - é a chave que vou referenciar, por exemplo, o ID da tabela files
-
-//   onUpdate: 'CASCADE',
-//   onDelete: 'SET NULL',
-//   allowNull: true,
-// },
+//   |_ por exemplo, eu quero que este campo referencie a tabela cities
+//   # key - é a chave que vou referenciar, por exemplo, o ID da tabela cities
